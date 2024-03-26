073887c42e52b7d4426737fd79921ad1fa488fa5
VigneshWaran-4 <mvikey2002@gmail.com>
2024-03-26 23:46:25 +0530

    first commit

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>About our company and products...</p>
+    </div>
+  );
+}
+
+export default About;
